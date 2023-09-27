28ff852429ce71e414e3f40763603a277269b96d
amaisuarez <amai1suarez55@gmail.com>
2023-09-27 11:15:44 +0100

    Last commit

--- 5. ArrayList and 2D Arrays/2D Arrays/5.3 Chessboard/src/Chessboard.java
+++ 5. ArrayList and 2D Arrays/2D Arrays/5.3 Chessboard/src/Chessboard.java
@@ -1,9 +1,31 @@
 public class Chessboard
 {
-   public static void main(String[] args)
-   {
+   public static void main(String[] args) {
       char[][] chessboard = new char[8][8];
 
-      /* Create and print a chessboard. */
+      for (int y = 0; y < chessboard.length; y++) {
+         for (int x = 0; x < chessboard.length; x++) {
+
+            if ((x + y) % 2 == 0) {
+               chessboard[y][x] = '\u25A1';
+            }else{
+                  chessboard[y][x] = '\u25A0';
+
+            }
+         }
+      }
+
+
+      for (char[] row: chessboard) {
+
+         for (char square: row) {
+
+
+
+            System.out.print(square);
+         }
+         System.out.println();
+      }
    }
 }
+
--- 5. ArrayList and 2D Arrays/ArrayList/5.2 Average Calculator/src/AverageCalculator.java
+++ 5. ArrayList and 2D Arrays/ArrayList/5.2 Average Calculator/src/AverageCalculator.java
@@ -2,8 +2,16 @@ import java.util.ArrayList;
 
 public class AverageCalculator
 {
-    public static double getAverageOf (ArrayList<Integer> numbers)
-    {
-        /* TODO */
+    public static double getAverageOf (ArrayList<Integer> numbers) {
+        double sum = 0;
+        for (int number : numbers) { //for number in numbers
+
+            sum += number; //we sum the numbers in our value sum.
+
+
+
+
+        }
+        return sum / numbers.size(); //we return the average with decimals of the ArrayList
     }
 }
